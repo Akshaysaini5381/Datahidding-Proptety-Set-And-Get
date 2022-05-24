2330e537ccbc8932812949e39a00bb72f06eb1fd
Akshaysaini5381 <akshaysaini5381@gmail.com>
2022-05-24 23:38:47 +0530

    Add project files.

---
+++ Datahidding Proptety Set And Get/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datahidding_Proptety_Set_And_Get
+{
+
+    class Data
+    {
+        private string  name;
+
+        public string show
+        {
+            set
+            {
+                this.name = value;
+            }
+            get
+            {
+                return name;
+            }
+        }
+      
+       
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Data obj = new Data();
+           
+            string n = obj.show = "This is my proparty And get set use";
+            Console.WriteLine(n);
+            Console.ReadLine();
+        }
+    }
+}
